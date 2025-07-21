0a96b073c63e668e8a1509ff6efba39867a6d50f
myfengshuireport <myfengshuireport@gmail.com>
2025-07-21 20:34:44 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  //base: "/feng-shui-finder-reveal-70/", // ← ADD THIS LINE
   base: mode === "production" ? "/feng-shui-finder-reveal-70/" : "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
